ce1a6f784f0b9a9c1d4e576b0164cd727285b0d1
laqiiz <mano.junki@gmail.com>
2021-05-15 11:29:54 +0900

    #632 Pocket, Hatebuの依存を排除

---
+++ scripts/social_button.js
@@ -0,0 +1,37 @@
+const fetch = require('sync-fetch')
+
+let pocketCnt = {};
+let hatebuCnt = {};
+
+
+hexo.extend.helper.register("get_pocket_count", (url) => {
+  const count = pocketCnt[url];
+  if (count) {
+    return count
+  }
+  console.log("start fetch pocket count");
+
+  let pocketURL = `https://widgets.getpocket.com/api/saves?url=${url}`
+  const saveCnt = fetch(pocketURL).json().saves;
+  pocketCnt[url] = saveCnt;
+
+  console.log(`finish pocket ${url}`);
+
+  return saveCnt;
+});
+
+hexo.extend.helper.register("get_hatebu_count", (url) => {
+  const count = hatebuCnt[url];
+  if (count) {
+    return count
+  }
+  console.log("start fetch hatebu count");
+
+  let hatebuURL = `https://bookmark.hatenaapis.com/count/entry?url=${encodeURI(url)}`
+  const bookmarkCnt = fetch(hatebuURL).json();
+  hatebuCnt[url] = bookmarkCnt;
+
+  console.log(`finish hatebu ${url}`);
+
+  return bookmarkCnt;
+});
