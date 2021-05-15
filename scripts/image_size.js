34b5ed7fd336124695162f33638a6dd312c9b066
laqiiz <mano.junki@gmail.com>
2021-05-15 22:40:15 +0900

    #668 記事一覧のサムネイルのimgタグにwidth,heightを追加

---
+++ scripts/image_size.js
@@ -0,0 +1,9 @@
+// https://www.npmjs.com/package/image-size
+const sizeOf = require('image-size');
+
+const currentDir = process.cwd();
+
+hexo.extend.helper.register("image_size_attribute", (path) => {
+  const dimensions = sizeOf(currentDir + "/source/" + path)
+  return `width=${dimensions.width} height=${dimensions.height}`;
+});
