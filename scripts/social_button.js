a0c33524ac0b37f2026e28404a438735bad0a711
laqiiz <mano.junki@gmail.com>
2021-05-15 22:20:51 +0900

    キャッシュ機構の導入

--- scripts/social_button.js
+++ scripts/social_button.js
@@ -1,8 +1,27 @@
-const fetch = require('sync-fetch')
+const fetch = require('sync-fetch');
+const fs = require("fs");
 
 let pocketCnt = {};
 let hatebuCnt = {};
 
+if (fs.existsSync("cache_pocket.json")) {
+  let pocketCache = fs.readFileSync("cache_pocket.json", 'utf-8');
+  if (pocketCache) {
+    pocketCnt = JSON.parse(pocketCache);
+  }
+}
+
+if (fs.existsSync("cache_hatebu.json")) {
+  let hatebuCache = fs.readFileSync("cache_hatebu.json", 'utf-8');
+  if (hatebuCache) {
+    hatebuCnt = JSON.parse(hatebuCache);
+  }
+}
+
+process.on('exit', function() {
+  fs.writeFileSync("cache_pocket.json", JSON.stringify(pocketCnt, null, 2));
+  fs.writeFileSync("cache_hatebu.json", JSON.stringify(hatebuCnt, null, 2));
+});
 
 hexo.extend.helper.register("get_pocket_count", (url) => {
   const count = pocketCnt[url];
@@ -35,3 +54,4 @@ hexo.extend.helper.register("get_hatebu_count", (url) => {
 
   return bookmarkCnt;
 });
+
