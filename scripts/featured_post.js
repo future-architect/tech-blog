ff190053f25e3db330421b5e891acba8e1884596
laqiiz <mano.junki@gmail.com>
2021-05-20 23:01:08 +0900

    #743 注目の記事のSNSカウントを表示

--- scripts/featured_post.js
+++ scripts/featured_post.js
@@ -1,24 +1,25 @@
 var gen = require('random-seed');
 
-hexo.extend.helper.register('featured_posts', function(seed, rate) {
-  if (!seed) {
-    seed = 'future';
-  }
+hexo.extend.helper.register('featured_posts', function(url, rate) {
+  url = url || 'future'; // seed value
+
   let count = 0;
 
-  var rand = gen.create(seed);
+  var rand = gen.create(url);
 
   const featureds = this.site.posts.reverse()
     .filter(post => post.featured == true && rand(100) < rate)
     .filter(_  => count++ < this.theme.featured_count)
     ;
 
+  const snsCnt = hexo.extend.helper.get('totalSNSCnt').bind(hexo);
+
   const links = featureds.map(post => `
-    <li>${post.date.format('YYYY.MM.DD')}<a href="/${post.path}" title="${post.lede}">${post.title}</a></li>`).join("\n")
+    <li><span>${post.date.format('YYYY.MM.DD')}</span><span class="snscount">♥${snsCnt(post.permalink)}</span> <a href="/${post.path}" title="${post.lede}">${post.title}</a></li>`).join("\n")
 
   return `
   <div class="widget">
-    <ul class="nav">
+    <ul class="nav featured-post-link">
       ${links}
     </ul>
   </div>
