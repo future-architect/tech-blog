53c856b755c75c09cd6d26585988cbc5d818f36e
laqiiz <mano.junki@gmail.com>
2021-05-27 01:21:46 +0900

    h1タグのanchorが表示されなくなったのでmarkdに切り戻し、footnotesのデグレを防ぐためカスタムscripts化

---
+++ scripts/footnotes.js
@@ -0,0 +1,72 @@
+'use strict';
+
+// Register footnotes filter
+hexo.extend.filter.register('before_post_render', function(data) {
+  data.content = renderFootnotes(data.content);
+  return data;
+});
+
+var md = require('markdown-it')({
+  html: true,   // allow HTML tags
+  linkify: true        // Autoconvert URL-like text to links
+});
+
+/**
+ * Render markdown footnotes
+ * @param {String} text
+ * @returns {String} text
+ */
+function renderFootnotes(text) {
+  var footnotes = [];
+  var reFootnoteContent = /\[\^(\d+)\]: ?([\S\s]+?)(?=\[\^(?:\d+)\]|\n\n|$)/g;
+  var reInlineFootnote = /\[\^(\d+)\]\((.+?)\)/g;
+  var reFootnoteIndex = /\[\^(\d+)\]/g;
+  var html = '';
+
+  // threat all inline footnotes
+  text = text.replace(reInlineFootnote, function(match, index, content) {
+    footnotes.push({
+      index: index,
+      content: content
+    });
+    // remove content of inline footnote
+    return '[^' + index + ']';
+  });
+  // threat all footnote contents
+  text = text.replace(reFootnoteContent, function(match, index, content) {
+    footnotes.push({
+      index: index,
+      content: content
+    });
+    // remove footnote content
+    return '';
+  });
+  // render (HTML) footnotes reference
+  text = text.replace(reFootnoteIndex,
+    '<sup id="fnref:$1"><a href="#fn:$1" rel="footnote">$1</a></sup>');
+  // sort footnotes by their index
+  footnotes.sort(function(a, b) {
+    return a.index - b.index;
+  });
+  // render footnotes (HTML)
+  footnotes.forEach(function(footNote) {
+    html += '<li id="fn:' + footNote.index + '">';
+    html += '<span style="vertical-align: top; padding-right: 10px;">';
+    html += footNote.index;
+    html += '.</span>';
+    html += '<span style="vertical-align: top;">';
+    html += md.renderInline(footNote.content.trim());
+    html += '</span>';
+    html += '<a href="#fnref:' + footNote.index + '" rev="footnote"> ↩</a>';
+    html += '</li>';
+  });
+  // add footnotes at the end of the content
+  if (footnotes.length) {
+    text += '<div id="footnotes">';
+    text += '<hr>';
+    text += '<div id="footnotelist">';
+    text += '<ol style="list-style:none; padding-left: 0;">' + html + '</ol>';
+    text += '</div></div>';
+  }
+  return text;
+}
