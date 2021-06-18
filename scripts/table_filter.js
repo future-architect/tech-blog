477980b3c0564737e47b6f57301024884aaa110e
laqiiz <mano.junki@gmail.com>
2021-06-19 01:15:56 +0900

    #947 全部表示できない場合はスクロール

---
+++ scripts/table_filter.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// hexo-renderer-marked はで拡張できる
+// https://github.com/hexojs/hexo-renderer-marked
+//
+// もとの markd を参考に拡張する
+// https://github.com/markedjs/marked/blob/e5796ecc435a30f96939e6a7b2229c14264b4bf8/src/Renderer.js#L92
+hexo.extend.filter.register('marked:renderer', function(renderer) {
+  renderer.table = function(header, body) {
+    return `<div class="scroll">${table(header, body)}</div>`;
+  };
+});
+
+const table = (header, body) => {
+  if (body) body = '<tbody>' + body + '</tbody>';
+
+  return '<table>\n'
+    + '<thead>\n'
+    + header
+    + '</thead>\n'
+    + body
+    + '</table>\n';
+}
