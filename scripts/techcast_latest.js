dba92fc93fa18b831b129b465b163b1b0448c3d5
laqiiz <mano.junki@gmail.com>
2021-09-22 23:25:41 +0900

    Tech Castを3件表示するように修正

--- scripts/techcast_latest.js
+++ scripts/techcast_latest.js
@@ -16,14 +16,13 @@ rssParser.parseURL('https://anchor.fm/s/2890e980/podcast/rss')
 
 hexo.extend.helper.register('generate_techcast_post', function() {
 
-  const feedHTML = feedItems.slice(0, 2).map(item => `<li><a href="${item.link}" title="フューチャーがお届けするポッドキャストです。${item.title}" target="_blank" rel="noopener">${item.title}</a></li>`).join("\n");
+  const feedHTML = feedItems.slice(0, 3).map(item => `<li><a href="${item.link}" title="フューチャーがお届けするポッドキャストです。${item.title}" target="_blank" rel="noopener">${item.title}</a></li>`).join("\n");
 
   return `
   <div class="class="widget-wrap">
   <div class="widget">
     <ul class="nav">
       ${feedHTML}
-    <li><a href="https://anchor.fm/futuretechcast/" title="Future Tech Castホーム" target="_blank" rel="noopener">...</a></li>
     </ul>
   </div>
   </div>
