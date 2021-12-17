4187f223df3d38c5349f86bcfb99f43e53f245cf
laqiiz <mano.junki@gmail.com>
2021-12-17 16:42:14 +0900

    software design - cypress

--- scripts/sns_count.js
+++ scripts/sns_count.js
@@ -2,7 +2,7 @@
 
 const fetch = require('sync-fetch');
 const {saveCache, pocket, hatebu, fb, tw, setTwitterCnt, setFacebookCnt, setHatebuCnt, setPocketCnt, getSNSCnt} = require('./lib/sns');
-const BEFORE_DATE = 7; // N日前までさかのぼってキャッシュ更新
+const BEFORE_DATE = 300; // N日前までさかのぼってキャッシュ更新
 const FETCH_DISABLE = process.env.FETCH_DISABLE === "true";
 let [currentPocket, currentHatebu, currentFb, currentTw] = [{}, {}, {}, {}];
 
