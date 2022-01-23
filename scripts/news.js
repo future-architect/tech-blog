4c32d75716f8bb9e8ccfebf8e672ca8982b0a4cc
laqiiz <mano.junki@gmail.com>
2022-01-23 16:32:30 +0900

    fx #1394 NEWSに表示される勉強会の日付を開催日に変更

--- scripts/news.js
+++ scripts/news.js
@@ -23,12 +23,22 @@ connpassFeedParser.parseURL('https://future.connpass.com/ja.atom')
 });
 
 hexo.extend.helper.register('generate_qiita_orgs', function() {
+  // connpassの場合はRSSを公開した日ではなく、イベントの開催日にしたい
+  const eventDate = feedItem => {
+    if (!feedItem.link.includes("connpass")) {
+      return feedItem.pubDate;
+    }
+    // summary: '開催日時: 2021/05/21 19:00 ～ 20:00<br />開催場所: オンライン<br /> といった形式で格納される
+    // eventDateStr is like format: 2021-09-03T19:00
+    const eventDateStr = feedItem.summary.split(" ～ ")[0].replaceAll("開催日時: ", "").replaceAll(" ", "T").replaceAll("/", "-");
+    return eventDateStr + ":00.000Z";
+  }
 
   const feeds = qiitaFeed.concat(connpassFeed);
   feeds.sort((a, b) => {
-    if (a.pubDate < b.pubDate) {
+    if (eventDate(a) < eventDate(b)) {
       return 1;
-    } else if (a.pubDate > b.pubDate) {
+    } else if (eventDate(a) > eventDate(b)) {
       return -1;
     } else {
       return 0;
@@ -44,7 +54,7 @@ hexo.extend.helper.register('generate_qiita_orgs', function() {
   }
 
   const newsDate = item => {
-    return item.isoDate.split("T")[0];
+    return eventDate(item).split("T")[0];
   }
 
   const feedHTML = feeds.slice(0, 5).map(item => `<li><span class="news-date">${newsDate(item)}</span><span class="news-category">${newsCategory(item)}</span><a href="${item.link}" title="${item.title}" target="_blank" rel="noopener" class="news-title">${item.title}</a></li>`).join("\n");
